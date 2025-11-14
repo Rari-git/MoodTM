21eaf0d6fa8beb518fc9b4b326c791aff02da4e5
Rari-git <staneanrares@gmail.com>
2025-11-14 15:56:38 +0200

    Initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
+import HomeHeader from "../components/HomeHeader";
+import SectionTitle from "../components/SectionTitle";
+import WidgetCard from "../components/WidgetCard";
 
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Radu</Text>
-    </View>
+    <ScrollView style={{ flex: 1, padding: 20, backgroundColor: "#fff" }}>
+      <HomeHeader />
+
+      <SectionTitle text="Recomandări pentru tine" />
+
+      <View style={{ gap: 16 }}>
+        <WidgetCard
+          title="Workout Daily"
+          subtitle="Antrenament rapid de 10 minute"
+        />
+        <WidgetCard
+          title="Mindfulness"
+          subtitle="Exercițiu simplu pentru relaxare"
+        />
+        <WidgetCard
+          title="Track Mood"
+          subtitle="Notează-ți starea zilei"
+        />
+      </View>
+    </ScrollView>
   );
 }
