bd7cc4878bfb8b9d216b86bfed05f141b2bb0142
Rari-git <staneanrares@gmail.com>
2025-11-15 16:12:51 +0200

    Initial commit

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,8 @@
 import { Text, View } from "react-native";
-
 export default function Settings() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 32 }}>Settings</Text>
-    </View>
-  );
+return (
+<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+<Text style={{ fontSize: 32 }}>Settings Page</Text>
+</View>
+);
 }
