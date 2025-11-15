619e284ab4a6c4865bebedc78f3c857e51c14d2e
Rari-git <staneanrares@gmail.com>
2025-11-15 11:00:46 +0200

    Initial commit

--- store/useMood.ts
+++ store/useMood.ts
@@ -1,26 +1,12 @@
 import { create } from "zustand";
-import { MoodEntry, moodService, MoodType } from "../api/moodService";
+import type { Mood } from "../constants/moodColors";
 
-interface MoodStore {
-  moods: MoodEntry[];
-  loading: boolean;
-  fetchMoods: () => Promise<void>;
-  addMood: (mood: MoodType, note?: string) => Promise<void>;
-}
+type MoodState = {
+  mood: Mood;
+  setMood: (m: Mood) => void;
+};
 
-export const useMoodStore = create<MoodStore>((set) => ({
-  moods: [],
-  loading: false,
-
-  fetchMoods: async () => {
-    set({ loading: true });
-    const moods = await moodService.getMoods();
-    set({ moods, loading: false });
-  },
-
-  addMood: async (mood, note) => {
-    const newMood = await moodService.addMood(mood, note);
-    set((state) => ({ moods: [newMood, ...state.moods] }));
-  },
+export const useMood = create<MoodState>((set) => ({
+  mood: "happy",
+  setMood: (m) => set({ mood: m }),
 }));
-
